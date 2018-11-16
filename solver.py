cb7620de2f3dc40e7512493ba32df0f53db54566
Christopher Liu <chrisliu@berkeley.edu>
2018-11-15 20:32:23 -0800

    solver

--- solver.py
+++ solver.py
@@ -1,5 +1,6 @@
 import networkx as nx
 import os
+import random
 
 ###########################################
 # Change this variable to the path to 
@@ -59,22 +60,57 @@ def minimum_cut(graph):
 
 def get_oversized_bus(buses, bus_size):
     for num, bus in enumerate(buses):
-        if sum([len(group) for group in bus]) > bus_size:
+        if len(bus) > bus_size:
             return num
     return -1
 
 def get_rowdy_groups(bus, constraints):
     rowdy_groups = []
     for rowdy_group in constraints:
-        
+        if rowdy_group.issubset(bus):
+            rowdy_groups.append(rowdy_group)
+    return rowdy_groups
+
+def score_bus(graph, bus, constraints):
+    invalid = set()
+
+    for rowdy_group in constraints:
+        if rowdy_group.issubset(bus):
+            invalid.update(rowdy_group)
+
+    score = 0
+    for kid in sorted(bus.difference(invalid)):
+        for neighbor in graph.neighbors(kid):
+            if neighbor in bus and neighbor not in invalid and neighbor > kid:
+                score += 1
+    return score
 
 def solve(graph, num_buses, bus_size, constraints):
     #TODO: Write this method as you like. We'd recommend changing the arguments here as well
+
+    '''
+    # random
+    buses = [set() for _ in range(num_buses)]
+    for kid in graph.nodes():
+        bus_num = random.randrange(num_buses)
+        while len(buses[bus_num]) == bus_size:
+            bus_num = random.randrange(num_buses)
+        buses[bus_num].add(kid)
+
+    return [[kid for kid in bus] for bus in buses]
+    '''
+
     if len(graph.nodes()) < num_buses:
         raise Exception('number of nodes less than number of buses')
 
     # make copy of graph
     copy = graph.copy()
+
+    # turn rowdy groups into sets
+    new_constraints = []
+    for constraint in constraints:
+        new_constraints.append(set(constraint))
+    constraints = new_constraints
 
     # disconnected graph until there are num_buses connected components
     while nx.number_connected_components(copy) < num_buses:
@@ -85,16 +121,90 @@ def solve(graph, num_buses, bus_size, constraints):
     buses = []
     for group in nx.connected_components(copy):
         if len(buses) < num_buses:
-            buses.append([group])
+            buses.append(group)
         else:
             for bus in buses:
-                if len(group) + sum([len(bus_group) for bus_group in bus]) <= bus_size:
-                    bus.append(group)
+                if len(group) + sum(len(bus)) <= bus_size:
+                    bus.update(group)
                     break
 
-    oversized = get_oversized_bus(buses, constraints)
+    oversized = get_oversized_bus(buses, bus_size)
     while oversized >= 0:
-        rowdy_groups = get_rowdy_groups()
+        '''
+        # remove people from rowdy groups first
+        rowdy_groups = get_rowdy_groups(buses[oversized], constraints)
+        # sort by descending rowdy group size
+        rowdy_groups = sorted(rowdy_groups, key=lambda x: len(x))
+        for rowdy_group in rowdy_groups:
+            # get kid with lowest degree in rowdy group
+            removed_kid = None
+            cost = 999999999
+            for kid in rowdy_group.intersection(buses[oversized]):
+                degree = copy.degree(kid)
+                if degree < cost:
+                    cost = degree
+                    removed_kid = kid
+
+            # bonus from removing removed_kid from bus
+            bonus = 0
+            for kid in sorted(rowdy_group):
+                if kid == removed_kid:
+                    continue
+                for neighbor in graph.neighbors(kid):
+                    if neighbor in buses[oversized] and neighbor != removed_kid and neighbor > kid:
+                        bonus += 1
+            
+            best_bus = None
+            best_malus = 999999999
+            for num, bus in enumerate(buses):
+                if num == oversized or len(bus) == bus_size:
+                    continue
+                score = score_bus(graph, bus, constraints)
+                malus = score - score_bus(graph, bus.union(set(kid)), constraints)
+                if malus < best_malus:
+                    best_bus = num
+                    best_malus = malus
+
+            if best_bus == None:
+                break
+
+            if best_malus < bonus:
+                print(removed_kid)
+                buses[best_bus].add(removed_kid)
+                buses[oversized].remove(removed_kid)
+
+        oversized = get_oversized_bus(buses, bus_size)
+        '''
+
+        bus = buses[oversized]
+        removed_kid = None
+        cost = 999999999
+        for kid in bus:
+            degree = sum([1 if neighbor in bus else 0 for neighbor in graph.neighbors(kid)])
+            if degree < cost:
+                cost = degree
+                removed_kid = kid
+
+        best_bus = None
+        best_score = -999999999
+        for num, bus in enumerate(buses):
+            if num == oversized or len(bus) == bus_size:
+                continue
+            score = score_bus(graph, bus.union(set(removed_kid)), constraints)
+            if score > best_score:
+                best_score = score
+                best_bus = num
+
+        if best_bus == None:
+            break
+
+        buses[best_bus].add(removed_kid)
+        buses[oversized].remove(removed_kid)
+
+        oversized = get_oversized_bus(buses, bus_size)
+
+    print(buses)
+    
 
 
     '''
@@ -106,8 +216,8 @@ def solve(graph, num_buses, bus_size, constraints):
     oversized = get_oversized_component(copy, bus_size)
     while oversized:
     '''
-    return [[]]
-        
+    
+    return [[kid for kid in bus] for bus in buses]
 
 
 def main():
